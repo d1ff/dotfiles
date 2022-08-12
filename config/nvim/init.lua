414186b71dc3b80f0d0759b2504958e41135fffc
Bornikov Vladislav <bortnikov.vladislav@e-sakha.ru>
2022-08-12 23:34:45 +0300

    Treesitter

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -62,6 +62,10 @@ require('packer').startup(function()
     use 'hrsh7th/cmp-vsnip'
     use 'hrsh7th/vim-vsnip'
     use "rafamadriz/friendly-snippets"
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
 end)
 
 local required_lsps = {
@@ -75,7 +79,8 @@ local required_lsps = {
     "marksman",
     "pyright",
     "sqlls",
-    "yamlls"
+    "yamlls",
+    "rust_analyzer"
 }
 
 require("mason").setup()
@@ -85,7 +90,7 @@ require("mason-lspconfig").setup({
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -94,27 +99,27 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
 end
 
 local lsp_flags = {
@@ -166,7 +171,7 @@ xmap        S   <Plug>(vsnip-cut-text)
 -- airline
 vim.g.airline_theme = 'solarized'
 vim.g.airline_powerline_fonts = 1
-vim.g.tmuxline_theme= 'airline'
+vim.g.tmuxline_theme = 'airline'
 vim.g.tmuxline_preset = 'powerline'
 vim.g.tmuxline_powerline_separators = 1
 vim.g.promptline_powerline_symbols = 1
@@ -263,8 +268,8 @@ api.nvim_set_keymap("n", "<leader>b", ":Buffers<CR>", { silent = true })
 
 -- nmap <leader>er :Error<CR>
 
-api.nvim_set_keymap("n", "<leader>nt", ":NERDTreeToggle %:p:%h<CR>", {noremap = true, silent = true})
-api.nvim_set_keymap("n", "<leader>tl", ":TagbarToggle<CR>", {noremap = true, silent = true})
+api.nvim_set_keymap("n", "<leader>nt", ":NERDTreeToggle %:p:%h<CR>", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "<leader>tl", ":TagbarToggle<CR>", { noremap = true, silent = true })
 
 -- Searching
 api.nvim_set_keymap("n", "/", "/\\v", { noremap = true })
@@ -402,18 +407,18 @@ au CompleteDone * pclose!
 nnoremap <leader>c :!cargo clippy
 ]])
 
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      end,
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -422,18 +427,18 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ['<Tab>'] = function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
         end,
         ['<S-Tab>'] = function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
         end
 
     }),
@@ -441,7 +446,7 @@ cmp.setup({
         { name = 'nvim_lsp' },
         { name = 'vsnip' }, -- For vsnip users.
     }, {
-    { name = 'buffer' },
+        { name = 'buffer' },
     })
 })
 
@@ -450,7 +455,7 @@ cmp.setup.filetype('gitcommit', {
     sources = cmp.config.sources({
         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
     }, {
-    { name = 'buffer' },
+        { name = 'buffer' },
     })
 })
 
@@ -482,4 +487,56 @@ for _, lsp in ipairs(required_lsps) do
         capabilities = capabilities
     }
 end
+
+--[[vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {]]
+--[[group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),]]
+--[[callback = function()]]
+--[[vim.opt.foldmethod     = 'expr']]
+--[[vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()']]
+--[[end]]
+--
+--[[})]]
+
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "devicetree",
+        "dockerfile",
+        "html",
+        "http",
+        "javascript",
+        "json",
+        "latex",
+        "lua",
+        "make",
+        "markdown",
+        "python",
+        "qmljs",
+        "regex",
+        "rust",
+        "sql",
+        "vim"
+    },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+}
 
