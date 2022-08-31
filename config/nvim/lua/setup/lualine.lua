b07c92e2210ee145cd9d9c2394cb33f7a8305bf9
Bornikov Vladislav <bortnikov.vladislav@e-sakha.ru>
2022-08-31 11:59:13 +0300

    winbar

--- config/nvim/lua/setup/lualine.lua
+++ config/nvim/lua/setup/lualine.lua
@@ -9,29 +9,43 @@ local function diff_source()
   end
 end
 
-require('lualine').setup {
-    options = {
-        theme = 'solarized',
-        globalstatus = true
-    },
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = { 'filename', 'diagnostics'},
-        lualine_c = { function()
-             return vim.fn['nvim_treesitter#statusline']("90")
-          end
-        },
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    },
-    tabline = {
-      lualine_a = {},
-      lualine_b = { { 'b:gitsigns_head', icon = '' }, {'diff', source = diff_source }},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {'buffers'},
-      lualine_z = {'tabs'}
-    }
+local winbar = {
+  lualine_a = {'filename' },
+  lualine_b = {{ 'diff', source = diff_source }},
+  lualine_c = {},
+  lualine_x = { function()
+     return vim.fn['nvim_treesitter#statusline']("90")
+  end},
+  lualine_y = {'diagnostics'},
+  lualine_z = {}
 }
+
+require('lualine').setup {
+  options = {
+      theme = 'solarized',
+      globalstatus = true,
+      disabled_filetypes = {
+        statusline = {},
+        winbar = { 'tagbar', 'NvimTree' },
+      },
+  },
+  sections = {
+      lualine_a = {'mode'},
+      lualine_b = { },
+      lualine_c = { },
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+  },
+  tabline = {
+    lualine_a = {},
+    lualine_b = { { 'b:gitsigns_head', icon = '' }, },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {'buffers'},
+    lualine_z = {'tabs'}
+  },
+  winbar = winbar,
+  inactive_winbar = winbar  
+} 
 
