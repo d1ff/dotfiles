ff638989926daf2c9e925bb325fa205324c5c480
Bornikov Vladislav <bortnikov.vladislav@e-sakha.ru>
2022-08-31 09:06:38 +0300

    treesitter in lualine

--- config/nvim/lua/setup/lualine.lua
+++ config/nvim/lua/setup/lualine.lua
@@ -1,6 +1,18 @@
 require('lualine').setup {
     options = {
         theme = 'solarized'
-    }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename',
+            function()
+               return vim.fn['nvim_treesitter#statusline']("90")
+            end
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
 }
 
