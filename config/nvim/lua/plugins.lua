2732f527b0d658d717cdaf91667070da848c69ad
Vladislav Bortnikov <bortnikov.vladislav@e-sakha.ru>
2022-09-06 10:34:06 +0300

    fixed typo

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -104,7 +104,7 @@ local function define_packages(use)
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-buffer'},
             { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-cmdline '},
+            { 'hrsh7th/cmp-cmdline'},
             { 'hrsh7th/cmp-vsnip' },
             { 'hrsh7th/vim-vsnip' },
             { "rafamadriz/friendly-snippets" },
